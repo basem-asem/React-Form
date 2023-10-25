a27c411657a0fee8d167a848aa272552bb9407ff
basem-asem <120086391+basem-asem@users.noreply.github.com>
2023-10-25 22:46:12 +0300

    add the adress and the date of birth

--- src/components/FormComponent.jsx
+++ src/components/FormComponent.jsx
@@ -8,6 +8,8 @@ const FormComponent = () => {
       name: '',
       email: '',
       phone: '',
+      address: '',
+      birthday: '',
     },
     validationSchema: Yup.object({
       name: Yup.string().required('Required'),
@@ -62,6 +64,32 @@ const FormComponent = () => {
         />
         {formik.touched.phone && formik.errors.phone ? (
           <div className="error">{formik.errors.phone}</div>
+        ) : null}
+
+        <label htmlFor="phone">Adress:</label>
+        <input
+          id="adress"
+          name="adress"
+          type="text"
+          onChange={formik.handleChange}
+          onBlur={formik.handleBlur}
+          value={formik.values.address}
+        />
+        {formik.touched.address && formik.errors.address ? (
+          <div className="error">{formik.errors.address}</div>
+        ) : null}
+
+        <label htmlFor="phone">Birthday:</label>
+        <input
+          id="birthday"
+          name="birthday"
+          type="date"
+          onChange={formik.handleChange}
+          onBlur={formik.handleBlur}
+          value={formik.values.phone}
+        />
+        {formik.touched.birthday && formik.errors.birthday ? (
+          <div className="error">{formik.errors.birthday}</div>
         ) : null}
 
         <button type="submit">Submit</button>
