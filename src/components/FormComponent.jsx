23d49e746b3f449b068d65e022ceaa9188a3f306
basem-asem <120086391+basem-asem@users.noreply.github.com>
2023-10-25 22:24:09 +0300

    make formik yup inputs

---
+++ src/components/FormComponent.jsx
@@ -0,0 +1,73 @@
+import { useFormik } from 'formik';
+import * as Yup from 'yup';
+import './FormComponent.css';
+
+const FormComponent = () => {
+  const formik = useFormik({
+    initialValues: {
+      name: '',
+      email: '',
+      phone: '',
+    },
+    validationSchema: Yup.object({
+      name: Yup.string().required('Required'),
+      email: Yup.string().email('Invalid email address').required('Required'),
+      phone: Yup.string().required('Required'),
+    }),
+    onSubmit: (values, { resetForm }) => {
+      // You can handle form submission here (e.g., send data to a server).
+      // For now, let's just show a success message and clear the form.
+      alert('Form submitted successfully!');
+      resetForm();
+    },
+  });
+
+  return (
+    <div className="form-container">
+      <form onSubmit={formik.handleSubmit}>
+        <label htmlFor="name">Name:</label>
+        <input
+          id="name"
+          name="name"
+          type="text"
+          onChange={formik.handleChange}
+          onBlur={formik.handleBlur}
+          value={formik.values.name}
+        />
+        {formik.touched.name && formik.errors.name ? (
+          <div className="error">{formik.errors.name}</div>
+        ) : null}
+
+        <label htmlFor="email">Email:</label>
+        <input
+          id="email"
+          name="email"
+          type="email"
+          onChange={formik.handleChange}
+          onBlur={formik.handleBlur}
+          value={formik.values.email}
+        />
+        {formik.touched.email && formik.errors.email ? (
+          <div className="error">{formik.errors.email}</div>
+        ) : null}
+
+        <label htmlFor="phone">Phone:</label>
+        <input
+          id="phone"
+          name="phone"
+          type="text"
+          onChange={formik.handleChange}
+          onBlur={formik.handleBlur}
+          value={formik.values.phone}
+        />
+        {formik.touched.phone && formik.errors.phone ? (
+          <div className="error">{formik.errors.phone}</div>
+        ) : null}
+
+        <button type="submit">Submit</button>
+      </form>
+    </div>
+  );
+};
+
+export default FormComponent;
